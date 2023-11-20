f0a0b0e0f5b8ff4cf30a85364bafef183e3a9a36
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-11-21 01:49:49 +0700

    Added ViewPager2 and Onboarding Screens #2

---
+++ app/src/main/java/com/putragandad/urbanfarm/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,21 @@
+package com.putragandad.urbanfarm.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.putragandad.urbanfarm.R
+
+class ViewPagerFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentList = arrayListOf<Fragment>(
+
+        )
+    }
+}
