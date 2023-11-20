9ac18a8a2bed2ae0647747b65f01f7e9aa55d54b
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-11-21 00:38:33 +0700

    Starting implementing onboarding screen (with ViewPager2) #1
    Due to conflict and previous project structure is a bit mess, i have to restructure the project environment by almost undo-ing everything.
    Starting from now, i'll working on Onboarding Screen first and then move to the part after this one finished.
    
    For onboarding screen, i'm using ViewPager2.
    Shoutout to this YouTube tutorial which i follow https://www.youtube.com/watch?v=COZ3j8Dwlog

--- app/src/main/java/com/putragandad/urbanfarm/MainActivity.kt
+++ app/src/main/java/com/putragandad/urbanfarm/MainActivity.kt
@@ -2,7 +2,6 @@ package com.putragandad.urbanfarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.putragandad.urbanfarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,25 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(Beranda())
-
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.beranda -> replaceFragment(Beranda())
-                R.id.tanamanku -> replaceFragment(Tanamanku())
-                R.id.jualBeliPanen -> replaceFragment(JualBeliPanen())
-                R.id.profil -> replaceFragment(Profil())
-                else ->{
-                }
-            }
-            true
-        }
-    }
-
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
-        fragmentTransaction.commit()
     }
 }
