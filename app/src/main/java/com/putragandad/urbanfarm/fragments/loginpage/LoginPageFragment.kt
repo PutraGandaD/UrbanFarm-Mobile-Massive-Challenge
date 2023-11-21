97ca6d90f3f854e4f50693089b832934fe7c8ac5
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-11-22 03:30:33 +0700

    Added Login and Register placeholder (FE)

--- app/src/main/java/com/putragandad/urbanfarm/fragments/loginpage/LoginPageFragment.kt
+++ app/src/main/java/com/putragandad/urbanfarm/fragments/loginpage/LoginPageFragment.kt
@@ -1,4 +1,4 @@
-package com.putragandad.urbanfarm.fragments.landingpage
+package com.putragandad.urbanfarm.fragments.loginpage
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,17 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.putragandad.urbanfarm.R
-import com.putragandad.urbanfarm.databinding.FragmentLandingPageBinding
+import com.putragandad.urbanfarm.databinding.FragmentLoginPageBinding
 
-class LandingPageFragment : Fragment() {
-    private var _binding : FragmentLandingPageBinding? = null
+class LoginPageFragment : Fragment() {
+    private var _binding : FragmentLoginPageBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLandingPageBinding.inflate(inflater, container, false)
-
+        _binding = FragmentLoginPageBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
--- app/src/main/java/com/putragandad/urbanfarm/fragments/registerpage/RegisterPageFragment.kt
+++ app/src/main/java/com/putragandad/urbanfarm/fragments/registerpage/RegisterPageFragment.kt
@@ -1,4 +1,4 @@
-package com.putragandad.urbanfarm.fragments.landingpage
+package com.putragandad.urbanfarm.fragments.registerpage
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,16 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.putragandad.urbanfarm.R
-import com.putragandad.urbanfarm.databinding.FragmentLandingPageBinding
+import com.putragandad.urbanfarm.databinding.FragmentRegisterPageBinding
 
-class LandingPageFragment : Fragment() {
-    private var _binding : FragmentLandingPageBinding? = null
+class RegisterPageFragment : Fragment() {
+    private var _binding : FragmentRegisterPageBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLandingPageBinding.inflate(inflater, container, false)
+        _binding = FragmentRegisterPageBinding.inflate(inflater, container, false)
 
         return binding.root
     }
