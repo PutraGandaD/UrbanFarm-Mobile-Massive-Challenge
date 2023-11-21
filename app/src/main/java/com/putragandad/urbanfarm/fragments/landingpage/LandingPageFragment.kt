e5e822d68c9b02aab683411c58a6490011c5b7e3
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-11-21 22:06:07 +0700

    Finished Onboarding screens with ViewPager2 (FINAL)
    Finished adding Onboarding screens using ViewPager2 along with it's logic such as :
    1. Using SharedPreferences to only show Onboarding Screen when user launched the app for the very first time,
       Otherwise, it's not gonna show the onboarding screen and straight into Landing Page
    
    2. Utilizing NavGraph to fix navigation backstack issue
    
    Shout to to this guy from YouTube, https://www.youtube.com/watch?v=COZ3j8Dwlog&t=1s for it's clear and structured
    instructions about how to add Onboarding Screens to Android app.
    
    From now on, development will start to Landing Page and then main features/MVP of the app

---
+++ app/src/main/java/com/putragandad/urbanfarm/fragments/landingpage/LandingPageFragment.kt
@@ -0,0 +1,17 @@
+package com.putragandad.urbanfarm.fragments.landingpage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.putragandad.urbanfarm.R
+class LandingPageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_landing_page, container, false)
+    }
+}
--- app/src/main/java/com/putragandad/urbanfarm/onboarding/screens/ThirdScreen.kt
+++ app/src/main/java/com/putragandad/urbanfarm/onboarding/screens/ThirdScreen.kt
@@ -1,10 +1,12 @@
 package com.putragandad.urbanfarm.onboarding.screens
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.putragandad.urbanfarm.R
 import com.putragandad.urbanfarm.databinding.FragmentSecondScreenBinding
@@ -27,10 +29,17 @@ class ThirdScreen : Fragment() {
         }
 
         binding.btnDoneOb3.setOnClickListener{
-            // Landing Page unfinished
+            findNavController().navigate(R.id.action_viewPagerFragment_to_landingPageFragment)
+            onBoardingFinished()
         }
 
         return binding.root
+    }
 
+    private fun onBoardingFinished() {
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("Finished", true)
+        editor.apply()
     }
 }
