efc4efc56c242afb94ec579056f3d28286ae5606
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-12-12 03:13:24 +0700

    Successfully tested Login With Google using Firebase

--- app/src/main/java/com/putragandad/urbanfarm/fragments/landingpage/LandingPageFragment.kt
+++ app/src/main/java/com/putragandad/urbanfarm/fragments/landingpage/LandingPageFragment.kt
@@ -1,17 +1,31 @@
 package com.putragandad.urbanfarm.fragments.landingpage
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.putragandad.urbanfarm.R
+import com.putragandad.urbanfarm.activity.beranda.FragmentContainerNavbarActivity
 import com.putragandad.urbanfarm.databinding.FragmentLandingPageBinding
 
 class LandingPageFragment : Fragment() {
     private var _binding : FragmentLandingPageBinding? = null
     private val binding get() = _binding!!
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +33,66 @@ class LandingPageFragment : Fragment() {
     ): View? {
         _binding = FragmentLandingPageBinding.inflate(inflater, container, false)
 
+        // Get instance of Firebase
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext() , gso)
+
+        binding.btnGoogleLanding.setOnClickListener {
+            signInGoogle()
+        }
+
         return binding.root
+
+        // Check if user already login or not
+        if (auth.currentUser != null) {
+            goToDashboard()
+        }
+    }
+
+    private fun signInGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == Activity.RESULT_OK){
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(requireContext(), task.exception.toString() , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                goToDashboard()
+            }else{
+                Toast.makeText(requireContext(), it.exception.toString() , Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun goToDashboard() {
+        val intent = Intent(requireContext(), FragmentContainerNavbarActivity::class.java)
+        startActivity(intent)
     }
 }
