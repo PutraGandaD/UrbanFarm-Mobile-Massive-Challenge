a3440106622f84049639244f786c3eced9341611
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-12-12 05:53:38 +0700

    Get profile value from Firebase into Profile Page
    For CircleImageView, with help of 3rd party plugins

--- app/src/main/java/com/putragandad/urbanfarm/fragments/profilpage/ProfilPageFragment.kt
+++ app/src/main/java/com/putragandad/urbanfarm/fragments/profilpage/ProfilPageFragment.kt
@@ -1,60 +1,49 @@
 package com.putragandad.urbanfarm.fragments.profilpage
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.putragandad.urbanfarm.R
+import com.putragandad.urbanfarm.activity.landingpage.LandingPageActivity
+import com.putragandad.urbanfarm.databinding.FragmentProfilPageBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfilPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfilPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding : FragmentProfilPageBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil_page, container, false)
+        _binding = FragmentProfilPageBinding.inflate(inflater, container, false)
+
+        //get firebase instance
+        auth = FirebaseAuth.getInstance()
+
+        //get profil value from firebase
+        val profileName = auth.currentUser?.displayName
+        val profilePictureUrl = auth.currentUser?.photoUrl
+
+        //set profile value from data above
+        binding.tvProfileName.text = profileName
+        Glide.with(requireContext())
+            .load(profilePictureUrl)
+            .into(binding.ivProfileImage)
+
+        //logout
+        binding.btnLogout.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(requireContext(), LandingPageActivity::class.java))
+            requireActivity().finish()
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfilPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfilPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
