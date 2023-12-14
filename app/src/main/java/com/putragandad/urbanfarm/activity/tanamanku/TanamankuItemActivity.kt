a9b69d84a4287704a73e6eb05985299323000513
Dewangga <hamadakinose@gmail.com>
2023-12-14 18:20:30 +0700

    TanamankuPageActivity (Front-End Only, Done)

---
+++ app/src/main/java/com/putragandad/urbanfarm/activity/tanamanku/TanamankuItemActivity.kt
@@ -0,0 +1,12 @@
+package com.putragandad.urbanfarm.activity.tanamanku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.putragandad.urbanfarm.R
+
+class TanamankuItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tanamanku_item)
+    }
+}
