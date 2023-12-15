4bbc7e2610de7f1b730e753ae46c8aa05f68ab1d
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-12-15 20:03:47 +0700

    Added Detail Page for Tanamanku (placeholder)

--- app/src/main/java/com/putragandad/urbanfarm/activity/tanamanku/DetailTanamankuItemActivity.kt
+++ app/src/main/java/com/putragandad/urbanfarm/activity/tanamanku/DetailTanamankuItemActivity.kt
@@ -3,10 +3,15 @@ package com.putragandad.urbanfarm.activity.tanamanku
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.putragandad.urbanfarm.R
+import com.putragandad.urbanfarm.databinding.ActivityDetailTanamankuItemBinding
 
 class DetailTanamankuItemActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailTanamankuItemBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_tanamanku_item)
+        binding = ActivityDetailTanamankuItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
